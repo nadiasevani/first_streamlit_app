9ac0eb163cd48619be80a17636e4769831569dd9
Nadia Sevani <109680115+nadiasevani@users.noreply.github.com>
2022-07-20 11:29:57 -0400

    Update streamlit_app.py
    imported pandas
    read the csv file and put it into a dataframe, display it into streamlit.

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,7 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Smoothie 🥝🍇')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://nadiasevani-first-streamlit-app-streamlit-app-lfcfsi.streamlitapp.com/") #read csv and pull the data into a dataframe
+streamlit.dataframe(my_fruit_lsit) #display the dataframe to streamlit
